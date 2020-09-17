59d747eedf7f702aa55f06792684b6bee326e2d2
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-09-17 12:44:09 +0100

    splash screen style added

---
+++ src/screens/Splash/SplashStyle.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import Colors from '../../Theme/Colors';
+export default StyleSheet.create({
+    Container: {
+        backgroundColor: Colors.background,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flex: 1,
+    }
+})
