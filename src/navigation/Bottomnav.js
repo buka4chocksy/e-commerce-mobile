3a4a328c112ffead9e107a147cadd5382d543c99
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-09-18 10:46:53 +0100

    setup buttom tab navigator

--- src/navigation/Bottomnav.js
+++ src/navigation/Bottomnav.js
@@ -1,12 +1,39 @@
+import React ,{useState}from 'react'
+import { View, Text,Image ,TextInput,TouchableOpacity} from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import Dashboard from '../screens/Dashboard/Dashboard';
+import Colors from '../../src/Theme/Colors';
+import Home from '../assets/images/Vector.png'
 const Tab = createBottomTabNavigator();
 
-const BottomTab =()=> {
+const BottomTab =(props)=> {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
+    <Tab.Navigator
+    tabBarOptions={{
+      activeTintColor: 'red',
+      style:{
+        backgroundColor:Colors.textColor,
+        borderTopRightRadius:20,
+        borderTopLeftRadius:20,
+        padding:30
+      }
+    }}
+    >
+      <Tab.Screen 
+      name="Dashboard" 
+      options={{
+        tabBarLabel:"",
+        tabBarOptions:()=>{
+          
+        },
+        tabBarIcon: ({ color, size }) => (
+          <TouchableOpacity onPress={()=>{ alert('hello')}}>
+          <Image source={Home} />
+          </TouchableOpacity>
+        ),
+      }} 
+      component={Dashboard} />
+      
     </Tab.Navigator>
   );
 }
