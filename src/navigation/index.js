e508fdc69dbcc560eb2aafe4190e89b325eaf563
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2020-09-18 10:48:04 +0100

    buttom tab file

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -6,6 +6,7 @@ import Screen2 from '../screens/OnBoarding/Screen2/Screen2';
 import Screen3 from '../screens/OnBoarding/Screen3/Screen3';
 import Login from '../screens/Auth/Login/Login';
 import Register from '../screens/Auth/Register/Register';
+import BottomTab from '../navigation/Bottomnav'
 import { View } from 'react-native';
 
 let Stack = createStackNavigator()
@@ -21,6 +22,8 @@ const MainNavigation = (props)=>{
         <Stack.Screen  name={'Screen3'} component={Screen3}/>
         <Stack.Screen  name={'Login'} component={Login}/>
         <Stack.Screen  name={'Register'} component={Register}/>
+        <Stack.Screen name={'BottomTab'} component={BottomTab}/>
+
         </Stack.Navigator>
         </>
 
